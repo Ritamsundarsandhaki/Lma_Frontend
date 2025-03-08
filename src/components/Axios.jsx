2194ca1d1268ecafb70d466c8db7879624ae10fe
Ritamsundarsandhaki <lttritam@gmail.com>
2025-03-08 20:27:36 +0530

    first commit

---
+++ src/components/Axios.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+    withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
